d3e3d1d02b966cd01e4c1128055bb345f2bea423
ihslimn <ihslimn@gmail.com>
2023-06-27 14:21:59 +0300

    ADD: Image Tag

---
+++ dynamic-tags/jet-engine-do-macros-dynamic-tag-image.php
@@ -0,0 +1,46 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+Class Elementor_Dynamic_Tag_Jet_Engine_Do_Macros_Image extends \Elementor\Core\DynamicTags\Data_Tag {
+
+	public function get_name() {
+		return 'jet-engine-do-macros-tag-image';
+	}
+
+	public function get_title() {
+		return 'Do Macros - Image';
+	}
+
+	public function get_group() {
+		return Jet_Engine_Dynamic_Tags_Module::JET_GROUP;
+	}
+
+	public function get_categories() {
+		return [ 
+			\Elementor\Modules\DynamicTags\Module::IMAGE_CATEGORY
+		];
+	}
+
+	protected function register_controls() {
+		$this->add_control(
+			'macros_string',
+			[
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'label' => 'Macros string',
+			]
+		);
+	}
+
+	public function get_value( array $options = array() ) {
+		$macros_string   = $this->get_settings( 'macros_string' );
+
+		$result = jet_engine()->listings->macros->do_macros( $macros_string );
+
+		$result = \Jet_Engine_Tools::get_attachment_image_data_array( $result );
+
+		return $result;
+	}
+
+}
--- jet-engine-do-macros-tag.php
+++ jet-engine-do-macros-tag.php
@@ -3,7 +3,7 @@
  * Plugin Name: JetEngine - Do macros
  * Description: Add dynamic tag that processes entered macros.
  * Plugin URI:  
- * Version:     1.0.1
+ * Version:     1.0.2
  * Author:      
  * Author URI:  
  * Text Domain: jet-engine
@@ -17,21 +17,8 @@ if ( ! class_exists( 'Do_Macros_Tag' ) ) {
 	
 	class Do_Macros_Tag {
 
-		private $new_elementor = false;
-
 		public function __construct() {
-			add_action( 'plugins_loaded', array( $this, 'add_hooks' ) );
-		}
-
-		public function add_hooks() {
-
-			if ( defined( 'ELEMENTOR_VERSION' ) && version_compare( ELEMENTOR_VERSION, '3.5.0', '>=' ) ) {
-				$this->new_elementor = true;
-				add_action( 'elementor/dynamic_tags/register', array( $this, 'register_dynamic_tag' ) );
-			} else {
-				add_action( 'elementor/dynamic_tags/register_tags', array( $this, 'register_dynamic_tag' ) );
-			}
-
+			add_action( 'elementor/dynamic_tags/register_tags', array( $this, 'register_dynamic_tag' ) );
 		}
 
 		public function register_dynamic_tag( $dynamic_tags ) {
@@ -41,12 +28,10 @@ if ( ! class_exists( 'Do_Macros_Tag' ) ) {
 			}
 
 			require_once( __DIR__ . '/dynamic-tags/jet-engine-do-macros-dynamic-tag.php' );
+			require_once( __DIR__ . '/dynamic-tags/jet-engine-do-macros-dynamic-tag-image.php' );
 
-			if ( $this->new_elementor ) {
-				$dynamic_tags->register( new Elementor_Dynamic_Tag_Jet_Engine_Do_Macros() );
-			} else {
-				$dynamic_tags->register_tag( 'Elementor_Dynamic_Tag_Jet_Engine_Do_Macros' );
-			}
+			$dynamic_tags->register_tag( 'Elementor_Dynamic_Tag_Jet_Engine_Do_Macros' );
+			$dynamic_tags->register_tag( 'Elementor_Dynamic_Tag_Jet_Engine_Do_Macros_Image' );
 
 		}
 
